0cbfe806800b25e53a87478f38e10ef6963ccc47
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-24 21:55:10 +0300

    Assignment Resubmitted

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.querySelector('main#main').remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
+newHeader.id = 'victory';
 newHeader.innerHTML = 'Densel Esekon is the champion';
 
 document.body.appendChild(newHeader);
